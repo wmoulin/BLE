a01db993d74d8f5f9d02fe3cb9c161f19a5db120
moulin wilfried <moulin.wilfried@gmail.com>
2016-09-13 00:31:47 +0200

    test connexion

--- js/driver.js
+++ js/driver.js
@@ -2,37 +2,18 @@ var bluetoothDevice;
 var colorCharacteristic;
 
 // Static values
-var buf = new ArrayBuffer(16);
-var bufView = new Uint16Array(buf);
 
-var byte0 = 0xff, // Static header
-    byte1 = 0x55, // Static header
-    byte2 = 0x09, // len
-    byte3 = 0x00, // idx
-    byte4 = 0x02, // action
-    byte5 = 0x0a, // device
-    byte6 = 0x09, // port
-    byte7 = 0x64; // slot
-//dynamics values
-var byte8 = 0x00, // data
-    byte9 = 0x00, // data
-    byte10 = 0x00, // data
-    byte11 = 0x00; // data
-//End of message
-var byte12 = 0x0a,
-    byte13 = 0x00,
-    byte14 = 0x00,
-    byte15 = 0x00;
+
+var byteTest= [0xBA, // Static header
+    0xBA, // Static header
+    0x03, // len
+    0x00, // Red
+    0x00, // Green
+    0x00]; // Blue
+
+
 
 // Gestion de l'inversion par paire des bytes
-bufView[0] = byte1 << 8 | byte0;
-bufView[1] = byte3 << 8 | byte2;
-bufView[2] = byte5 << 8 | byte4;
-bufView[3] = byte7 << 8 | byte6;
-bufView[4] = byte9 << 8 | byte8;
-bufView[5] = byte11 << 8 | byte10;
-bufView[6] = byte13 << 8 | byte12;
-bufView[7] = byte15 << 8 | byte14;
 
 function onTouchEnd() {
   console.log("touchend");
@@ -58,9 +39,7 @@ function connect() {
   })
   .then(server => server.getPrimaryService(parseInt("0xBABA")))
   .then(service => service.getCharacteristic(parseInt("0xAAAA")))
-  /*.then(characteristic => {characteristic.writeValue(buf)*/
-
-  .then(characteristic => {
+    .then(characteristic => {
     let decoder = new TextDecoder('utf-8');
     return characteristic.readValue().then(value => {
       console.log("characteristic value :", value);
@@ -71,9 +50,6 @@ function connect() {
     //var newColor = new Uint8Array([1]);
     //return characteristic.writeValue(newColor);
   }).then(_ => {document.getElementById("connectBtn").onclick=function(){disconnect()};})
-  /*.then(_ => {
-    console.log('Energy expended has been reset.');
-  })*/
   .catch(error => { console.log(error); });
 }
 
@@ -85,8 +61,11 @@ function disconnect() {
 
   if (bluetoothDevice.gatt.connected) {
     console.log('Deconnexion');
-    bluetoothDevice.gatt.disconnect();
-    document.getElementById("connectBtn").onclick=function(){connect()};
+    colorCharacteristic.stopNotifications().then(_ => {
+      colorCharacteristic.removeEventListener('characteristicvaluechanged', callback);
+      bluetoothDevice.gatt.disconnect();
+      document.getElementById("connectBtn").onclick=function(){connect()};
+    });
   } else {
     console.log('Bluetooth Device is already disconnected');
   }
@@ -110,34 +89,10 @@ function hexValue(value) {
 }
 
 function sendColor() {
-  console.log("connexion");
-  navigator.bluetooth.requestDevice({
-    filters: [{
-      name: 'LEO',
-	    services: [parseInt("0xBABA")]
-    }]
+  characteristic.writeValue(inverseByte(byteTest))
+  .then( () => {
+    console.log('New color send.');
   })
-  .then(device => {
-    console.log("try connect");
-    device.addEventListener('gattserverdisconnected', onDisconnected);
-    device.gatt.connect();})
-  .then(server => { console.log("connect"); })
-  .then(server => server.getPrimaryService(parseInt("0xBABA")))
-  .then(service => service.getCharacteristic(parseInt("0xaaaa")))
-  .then(characteristic => {
-    let decoder = new TextDecoder('utf-8');
-	  colorCharacteristic = characteristic;
-	  return characteristic.readValue().then(value => {
-	    console.log("characteristic value :", decoder.decode(value));
-	  });
-
-    // Writing 1 is the signal to reset energy expended.
-    //var newColor = new Uint8Array([1]);
-    //return characteristic.writeValue(newColor);
-  }).then(_ => {document.getElementById("connectBtn");})
-  /*.then(_ => {
-    console.log('Energy expended has been reset.');
-  })*/
   .catch(error => { console.log(error); });
 
 }
@@ -168,4 +123,14 @@ function isDeviceinit() {
   if (!bluetoothDevice) {
     throw new Error("Bluetooth device not initialize !");
   }
+}
+
+function inverseByte(byteArray) {
+  var buf = new ArrayBuffer(byteArray.length);
+  var bufView = new Uint16Array(buf);
+  // Gestion de l'inversion par paire des bytes
+  for (int i = 0; i < byteArray.length; i+2) {
+    bufView[0] = byteArray[i+1] << 8 | byteArray[i];
+  }
+  return buf;
 }
